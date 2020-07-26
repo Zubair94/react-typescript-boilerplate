d00c15f576189c51bc951f5e14926403cfd487a8
Zubair94 <kazizubair1994@gmail.com>
2020-07-26 18:49:26 +0600

    Convert to react boilerplate
    - Update tsconifg
    - Add svg typings
    - Update packages
    - Add eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { App } from "./app/app";
-import "./styles/styles.scss";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './app/app';
+import './styles/styles.scss';
+
 ReactDOM.render(
-    <App compiler="TypeScript Yea" framework="React" />,
-    document.getElementById("root")
+    <React.StrictMode>
+        <App compiler="TypeScript" framework="React" />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
